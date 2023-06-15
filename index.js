d720ee612f5589aaa78922937f2c183b451850de
Salvador Xe <salvadorxe@salvadors-mbp.lan>
2023-06-14 21:45:28 -0400

    Cleaned up CSS and UI of page. Navbar 90% finished; missing the case for smaller screen. Starting the boilerplate of project page

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/*var aDiv = document.getElementById("expand");
 
+function changeWidth() 
+{
+    var scrollVal = window.scrollY
+    var scrollSlow  = (scrollVal / 4);
+    
+    //Changing CSS Width
+    aDiv.style.width = Math.min(Math.max(scrollSlow, 20), 100) + "%";
 
-      
+    aDiv.style.height = Math.min(Math.max(scrollSlow, 20), 100) + "%";
+}
+
+window.addEventListener('scroll', function() 
+{
+    requestAnimationFrame(changeWidth);
+}, false); 
+
+*/
+function myFunction(){
+    
+}
